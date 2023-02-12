e76e275e0237d6c53cb9992de6842830e93bb901
Tyomios <tyomios@mail.ru>
2023-02-12 22:15:08 +0700

    Добавление классов модели дерева и его представления.

---
+++ web-tree/web-tree/src/Tree.js
@@ -0,0 +1,57 @@
+export class Tree {
+    constructor(rootNode) {
+        this.rootNode = rootNode;
+    }
+
+    // Add node to tree.
+    addNode(node, parentId) {
+        let parentNode = this.findNodeById(parentId);
+        if (parentNode) {
+            parentNode.childs.push(node);
+        }
+    }
+
+    // Remove node from tree.
+    removeNode(id) {
+        let parentNode = this.findParentNode(id);
+        if (parentNode) {
+            let index = parentNode.childs.findIndex(node => node.id === id);
+            if (index !== -1) {
+                parentNode.childs.splice(index, 1);
+            }
+        }
+    }
+
+    // edit node in tree.
+    editNode(id, name) {
+        let node = this.findNodeById(id);
+        if (node) {
+            node.name = name;
+        }
+    }
+
+    // remove all childs from node.
+    clearNode() {
+        this.rootNode.childs = [];
+    }
+
+    // Find node by id.
+    findNodeById(id) {
+        return this.findNodeByIdRecursive(id, this.rootNode);
+    }
+
+    // Find node by id.
+    // Recursive function.
+    findNodeByIdRecursive(id, node) {
+        if (node.id === id) {
+            return node;
+        }
+
+        for (let i = 0; i < node.childs.length; i++) {
+            let foundNode = this.findNodeByIdRecursive(id, node.childs[i]);
+            if (foundNode) {
+                return foundNode;
+            }
+        }
+    }
+}
---
+++ web-tree/web-tree/src/TreeNodeView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.css';
+
+// Component to display tree node data and and fire events.
+export class TreeNodeView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected: false
+        };
+    }
+
+    // Handle click on node name.
+    // If node has childs, then expand/collapse node.
+    // If node has no childs, then fire event to parent component.
+    handleNodeClick = (event) => {
+        this.props.onNodeClick(this.props.node.id, this.props.node.name);
+    }
+
+    // Render node name and childs.
+    render() {
+        return (
+            <div>
+                <div className="node" onClick={this.handleNodeClick}>
+                    {this.props.node.name}
+                </div>
+                {this.props.node.childs.map((node, index) => {
+                    return <NodeView key={index} node={node} onNodeClick={this.props.onNodeClick} />
+                })}
+            </div>
+        );
+    }
+}
